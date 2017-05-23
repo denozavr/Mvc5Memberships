f657214a6f3fb26b4bbf951061281cc4365047c3
Den <denizdeniz90@gmail.com>
2017-05-23 14:24:43 +0300

    Add project files.

---
+++ Mvc5Memberships/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5Memberships.Startup))]
+namespace Mvc5Memberships
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
