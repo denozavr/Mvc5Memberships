3505f37e4058ba666636df7e003fad491cc7373c
Den <denizdeniz90@gmail.com>
2017-05-30 12:30:46 +0300

    Add Reflection extension method

---
+++ Mvc5Memberships/Extenstion/ReflectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5Memberships.Extenstion
+{
+    public static class ReflectionExtensions
+    {
+        public static string GetPropValue<T>(this T item, string propertyName)
+        {
+            return item.GetType().GetProperty(propertyName)?.GetValue(item, null).ToString();
+        }
+    }
+}
