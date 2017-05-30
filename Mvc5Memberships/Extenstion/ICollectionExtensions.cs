bd7be773f0e62ba276776e58cb938c1761b5d693
Den <denizdeniz90@gmail.com>
2017-05-30 12:58:03 +0300

    Implement ToSelectListItem extension for Dropdowns in views (Item model)

---
+++ Mvc5Memberships/Extenstion/ICollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5Memberships.Extenstion
+{
+    public static class ICollectionExtensions
+    {
+        public static IEnumerable<SelectListItem> ToSelectListItem<T>(this ICollection<T> items, int selected)
+        {
+            return from item in items
+                select new SelectListItem()
+                {
+                    Text = item.GetPropValue("Title"),
+                    Value = item.GetPropValue("Id"),
+                    Selected = item.GetPropValue("Id").Equals(selected.ToString())
+                };
+        }
+    }
+}
