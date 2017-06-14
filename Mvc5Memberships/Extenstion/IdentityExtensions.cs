7a2c87d54d8256fe4fa6bb43251716d623ecb8e0
Den <denizdeniz90@gmail.com>
2017-06-14 11:06:03 +0300

    Add extensions methods for Identity

---
+++ Mvc5Memberships/Extenstion/IdentityExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+using System.Web;
+using Mvc5Memberships.Models;
+
+namespace Mvc5Memberships.Extenstion
+{
+    public static class IdentityExtensions
+    {
+        public static string GetUserFirstName(this IIdentity identity)
+        {
+            var db = ApplicationDbContext.Create();
+            var userName = db.Users.FirstOrDefault(u => u.UserName == identity.Name)?.FirstName;
+
+            return userName ?? string.Empty;
+        }
+
+        public static async Task GetUsers(this List<UserViewModel> users)
+        {
+            var db = ApplicationDbContext.Create();
+            users.AddRange(await db.Users.Select(u => new UserViewModel
+            {
+                Id = u.Id,
+                Email = u.Email,
+                FirstName = u.FirstName
+            }).OrderBy(u => u.Email).ToListAsync());
+
+        }
+    }
+}
