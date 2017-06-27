6a0f7d151519e97a7bc52e469d11e3838ac14dac
Den <denizdeniz90@gmail.com>
2017-06-27 15:35:52 +0300

    Add Thumbnail JOIN extension method to fetch all data about thumbnail and use it in HomeController

--- Mvc5Memberships/Controllers/HomeController.cs
+++ Mvc5Memberships/Controllers/HomeController.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Mvc5Memberships.Extenstion;
+using Mvc5Memberships.Models;
 
 namespace Mvc5Memberships.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
             var userId = Request.IsAuthenticated ? HttpContext.GetUserIdCtx() : null;
+            var thumbs = await new List<ThumbnailModel>().GetProductThumbnailsAsync(userId);
+
             return View();
         }
 
--- Mvc5Memberships/Extenstion/ThumbnailExtensions.cs
+++ Mvc5Memberships/Extenstion/ThumbnailExtensions.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
+using Mvc5Memberships.Comparers;
 using Mvc5Memberships.Models;
 
 namespace Mvc5Memberships.Extenstion
@@ -29,5 +30,60 @@ namespace Mvc5Memberships.Extenstion
             return new List<int>();
         }
 
+
+        public static async Task<IEnumerable<ThumbnailModel>> GetProductThumbnailsAsync(
+            this List<ThumbnailModel> thumbnails, string userId = null, ApplicationDbContext db = null)
+        {
+            try
+            {
+                if (userId == null) return new List<ThumbnailModel>();
+                if (db == null) db = ApplicationDbContext.Create();
+
+                var subscriptionIds = await GetSubscriptionIdsAsync(userId, db);
+
+                thumbnails = await db.SubscriptionProducts.Join(db.Products, sp => sp.ProductId, p => p.Id,(sp, p) => new {sp,p})
+                            .Join(db.ProductLinkTexts, p2 => p2.p.ProductLinkTextId, plt => plt.Id, (p2,plt)=>new{p2,plt})
+                            .Join(db.ProductTypes, p3=> p3.p2.p.ProductTypeId, pt=>pt.Id, (p3,pt)=> new {p3,pt})
+                            .Where(c=> subscriptionIds.Contains(c.p3.p2.sp.SubscriptionId))
+                            .Select( s=> new ThumbnailModel()
+                            {
+                                ProductId = s.p3.p2.p.Id,
+                                SubscriptionId = s.p3.p2.sp.SubscriptionId,
+                                Title = s.p3.p2.p.Title,
+                                Description = s.p3.p2.p.Description,
+                                ImageUrl = s.p3.p2.p.ImageUrl,
+                                Link = "/ProductContent/Index/" + s.p3.p2.p.Id,
+                                TagText = s.p3.plt.Title,
+                                ContentTag = s.pt.Title
+                            }).ToListAsync();
+
+
+                //SQL syntax
+                //thumbnails = await (
+                //    from ps in db.SubscriptionProducts
+                //    join p in db.Products on ps.ProductId equals p.Id
+                //    join plt in db.ProductLinkTexts on p.ProductLinkTextId equals plt.Id
+                //    join pt in db.ProductTypes on p.ProductTypeId equals pt.Id
+                //    where subscriptionIds.Contains(ps.SubscriptionId)
+                //    select new ThumbnailModel
+                //    {
+                //        ProductId = p.Id,
+                //        SubscriptionId = ps.SubscriptionId,
+                //        Title = p.Title,
+                //        Description = p.Description,
+                //        ImageUrl = p.ImageUrl,
+                //        Link = "/ProductContent/Index/" + p.Id,
+                //        TagText = plt.Title,
+                //        ContentTag = pt.Title
+                //    }).ToListAsync();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return thumbnails.Distinct(new ThumbnailEqualityComparer()).OrderBy(o => o.Title);
+        }
     }
 }
