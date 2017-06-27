e69ed05f0dd9ce2898f2d161e8f8276cf7a8cd73
Den <denizdeniz90@gmail.com>
2017-06-27 13:07:01 +0300

    Add Thumbnail Comparer(using ID, not reference) and add ThumbnailExtension

---
+++ Mvc5Memberships/Comparers/ThumbnailEqualityComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mvc5Memberships.Models;
+
+namespace Mvc5Memberships.Comparers
+{
+    public class ThumbnailEqualityComparer : IEqualityComparer<ThumbnailModel>
+    {
+        //compare Thumbnails objects using ProdID not their reference
+        public bool Equals(ThumbnailModel thumb1, ThumbnailModel thumb2)
+        {
+            return thumb1.ProductId.Equals(thumb2.ProductId);
+        }
+
+        public int GetHashCode(ThumbnailModel thumb)
+        {
+            return thumb.ProductId;
+        }
+    }
+}
---
+++ Mvc5Memberships/Extenstion/ThumbnailExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Mvc5Memberships.Models;
+
+namespace Mvc5Memberships.Extenstion
+{
+    public static class ThumbnailExtensions
+    {
+        private static async Task<List<int>> GetSubscriptionIdsAsync(
+            string userId = null, ApplicationDbContext db = null)
+        {
+            try
+            {
+                if (userId == null) return new List<int>();
+                if (db == null) db = ApplicationDbContext.Create();
+
+                return await (db.UserSubscriptions.Where(us => us.UserId == userId)
+                    .Select(us => us.SubscriptionId)).ToListAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return new List<int>();
+        }
+
+    }
+}
