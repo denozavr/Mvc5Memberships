fb744df52426af2ac5c5824165abef89977b70ae
Den <denizdeniz90@gmail.com>
2017-07-26 17:10:35 +0300

    Add JS for carret

--- Mvc5Memberships/App_Start/BundleConfig.cs
+++ Mvc5Memberships/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace Mvc5Memberships
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/carret.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
