accf5b353c667ebdfb960c7ccf1685c7a36da541
Den <denizdeniz90@gmail.com>
2017-08-01 17:51:38 +0300

    Add ViewModel for content

---
+++ Mvc5Memberships/Models/ContentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvc5Memberships.Models
+{
+    public class ContentViewModel
+    {
+        public int ProductId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Html { get; set; }
+        public string VideoUrl { get; set; }
+    }
+}
