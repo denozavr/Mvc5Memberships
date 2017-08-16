f359d085f8ced76829568775ef67bc0470af63ee
Den <denizdeniz90@gmail.com>
2017-08-16 10:56:01 +0300

    Add GetSubscriptionIdByCode extension method

---
+++ Mvc5Memberships/Extenstion/SubscriptionExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mvc5Memberships.Entities;
+
+namespace Mvc5Memberships.Extenstion
+{
+    public static class SubscriptionExtensions
+    {
+        public static async Task<int> GetSubscriptionIdByRegistrationCode(
+            this IDbSet<Subscription> subscription, string code)
+        {
+            try
+            {
+                if (subscription == null || code == null || code.Length <= 0)
+                    return Int32.MinValue;
+
+                var subscriptionId = await subscription.FirstOrDefaultAsync(s => s.RegistrationCode == code);
+                var id = subscriptionId.Id;
+
+                return id;
+            }
+            catch { return Int32.MinValue; }
+        }
+    }
+}
