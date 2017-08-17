f101cfe0dec0f224bb1c581124167ecee65bc7fc
Den <denizdeniz90@gmail.com>
2017-08-17 16:04:38 +0300

    Add RegisterCodeController

---
+++ Mvc5Memberships/Controllers/RegisterCodeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc5Memberships.Controllers
+{
+    public class RegisterCodeController : Controller
+    {
+        // GET: RegisterCode
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
