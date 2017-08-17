bca6a36d0b27b5d9b5388f34442d879f38d74136
Den <denizdeniz90@gmail.com>
2017-08-17 16:27:14 +0300

    Add Register (Code) action method

--- Mvc5Memberships/Controllers/RegisterCodeController.cs
+++ Mvc5Memberships/Controllers/RegisterCodeController.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Mvc5Memberships.Extenstion;
 
 namespace Mvc5Memberships.Controllers
 {
     public class RegisterCodeController : Controller
     {
         // GET: RegisterCode
-        public ActionResult Index()
+        public async Task<ActionResult> Register(string code)
         {
-            return View();
+            if (Request.IsAuthenticated)
+            {
+                var userId = HttpContext.GetUserIdCtx();
+
+                var registred = await SubscriptionExtensions
+                    .RegisterUserSubscriptionCode(userId, code);
+
+                if (!registred) throw new ApplicationException();
+
+                return PartialView("_RegisterCodePartial");
+            }
+            return View("Error");
         }
     }
 }
