11a0adc530bed54f81cb2b4976fb12108c568d77
Den <denizdeniz90@gmail.com>
2017-08-31 15:52:35 +0300

    Add JS and Css for dropdown menu

--- Mvc5Memberships/Scripts/login.js
+++ Mvc5Memberships/Scripts/login.js
@@ -1,1 +1,9 @@
-﻿
+﻿$(function () {
+    //Wire up the hover event
+    var loginLinkHover = $("#loginLink").hover(onLoginLinkHover);
+
+    function onLoginLinkHover()
+    {
+        $("div[data-login-user-area]").addClass('open');
+    }
+});
