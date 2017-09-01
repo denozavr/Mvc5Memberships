48883fd639a1273a7f1eeccac392bca1742a813b
Den <denizdeniz90@gmail.com>
2017-09-01 12:44:46 +0300

    Add code to remove LoginPanel popup

--- Mvc5Memberships/Scripts/login.js
+++ Mvc5Memberships/Scripts/login.js
@@ -1,9 +1,13 @@
 ﻿$(function () {
     //Wire up the hover event
     var loginLinkHover = $("#loginLink").hover(onLoginLinkHover);
+    var loginCloseButton = $("#close-login").click(onCloseLogin);
 
-    function onLoginLinkHover()
-    {
-        $("div[data-login-user-area]").addClass('open');
+    function onLoginLinkHover() {
+        $("div[data-login-user-area]").addClass("open");
+    }
+
+    function onCloseLogin() {
+        $("div[data-login-user-area]").removeClass("open");
     }
 });
